64e6c977d39ef5cc1e49fdaaa314088f1a006bee
zhouman <mie.2013@qq.com>
2018-08-03 09:17:40 +0800

    version1.1

--- src/main/java/com/exercise/demo/tool/db.sql
+++ src/main/java/com/exercise/demo/tool/db.sql
@@ -26,14 +26,21 @@ CREATE TABLE sku(
   FOREIGN KEY(gid) REFERENCES goods(gid)
 )DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-insert into repertory values (1,'天猫一号仓','北京');
-insert into repertory values (2,'二号仓','上海');
-insert into repertory values (3,'三号仓','广州');
-insert into repertory values (4,'珠三角仓库','深圳');
-insert into repertory values (5,'山东仓','济南');
-insert into repertory values (6,'安徽集散','合肥');
-insert into repertory values (7,'江西集散','鹰潭');
+insert into repertory values (1,'tmall','Beijing');
+insert into repertory values (2,'taobao','Shanghai');
+insert into repertory values (3,'jingdong','Guangzhou');
+insert into repertory values (4,'alibaba','Shenzhen');
+insert into repertory values (5,'Amazon','Jinan');
+insert into repertory values (6,'ebay','Hefei');
+insert into repertory values (7,'asus','Yingtan');
 
+insert into goods values (1,'shirt');
+insert into goods values (2,'coat');
+insert into goods values (3,'sweater');
+insert into goods values (4,'shirt');
+insert into goods values (5,'coat');
+insert into goods values (6,'sweater');
+insert into goods values (7,'sweater');
 
 insert into relation values (1,1,1);
 insert into relation values (2,1,2);
@@ -43,13 +50,6 @@ insert into relation values (5,3,5);
 insert into relation values (6,4,6);
 insert into relation values (7,5,7);
 
-insert into goods values (1,'shirt');
-insert into goods values (2,'coat');
-insert into goods values (3,'sweater');
-insert into goods values (4,'shirt');
-insert into goods values (5,'coat');
-insert into goods values (6,'sweater');
-insert into goods values (7,'sweater');
 
 insert into sku values (1,1,'red','XL','new-style1');
 insert into sku values (2,2,'green','XL','new-style2');
